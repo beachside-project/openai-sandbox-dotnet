4b77ab5fac03bd42b7e06100bada0e14a28c1b38
Atsushi Yokohama <yokohama@beachside.dev>
2023-08-04 20:51:04 +0900

    Adding SK TextChunker sample

--- SemanticKernelSandbox/SemanticKernelSandbox.TextSplitSamples/Program.cs
+++ SemanticKernelSandbox/SemanticKernelSandbox.TextSplitSamples/Program.cs
@@ -4,8 +4,62 @@ namespace SemanticKernelSandbox.TextSplitSamples;
 
 internal class Program
 {
-    static void Main(string[] args)
+    // 結論:
+    // 英語だと maxTokensPerLine で示した token 数以内で分割してくれている。
+    // 日本語は全然だめかもしれん。maxTokensPerLine =200 に設定したら適当に400-700位で分割、500 にしたら2000オーバーする感じ。
+    private static async Task Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        //var filePath = @"C:\repos\azure-openai\openai-sandbox-dotnet\SemanticKernelSandbox\SemanticKernelSandbox.TextSplitSamples\english.txt";
+        var filePath = @"C:\repos\azure-openai\openai-sandbox-dotnet\SemanticKernelSandbox\SemanticKernelSandbox.TextSplitSamples\magica.md";
+
+        using var reader1 = new StreamReader(filePath);
+        var sampleText = reader1.ReadToEnd();
+
+        var sampleTexts = new List<string>();
+        using (var reader2 = new StreamReader(filePath))
+        {
+            while (true)
+            {
+                var line = reader2.ReadLine();
+                if (line == null) break;
+                sampleTexts.Add(line);
+            }
+        };
+
+        var maxTokensPerLine = 200;
+        var overlapTokens = 0;
+
+        // PlainText:
+
+        var t1 = TextChunker.SplitPlainTextLines(sampleText, maxTokensPerLine);
+        var t1TokensResult = await t1
+                            .Select(async chunk => new { length = chunk.Length, Tokens = await TokenCounter.CountTokenAsync(chunk) })
+                            .WhenAll();
+
+        var t2 = TextChunker.SplitPlainTextParagraphs(sampleTexts, maxTokensPerLine, overlapTokens);
+        var t2TokensResult = await t2
+                            .Select(async chunk => new { length = chunk.Length, Tokens = await TokenCounter.CountTokenAsync(chunk) })
+                            .WhenAll();
+
+        // Markdown:
+
+        var m1 = TextChunker.SplitMarkDownLines(sampleText, maxTokensPerLine);
+        var m1TokensResult = await m1
+                            .Select(async chunk => new { length = chunk.Length, Tokens = await TokenCounter.CountTokenAsync(chunk) })
+                            .WhenAll();
+
+        var m2 = TextChunker.SplitMarkdownParagraphs(sampleTexts, maxTokensPerLine, overlapTokens);
+        var m2TokensResult = await m2
+                            .Select(async chunk => new { length = chunk.Length, Tokens = await TokenCounter.CountTokenAsync(chunk) })
+                            .WhenAll();
     }
+}
+
+public static class TaskEnumerableExtensions
+{
+    public static Task WhenAll(this IEnumerable<Task> tasks) => Task.WhenAll(tasks);
+
+    public static Task<T[]> WhenAll<T>(this IEnumerable<Task<T>> tasks) => Task.WhenAll(tasks);
 }
---
+++ SemanticKernelSandbox/SemanticKernelSandbox.TextSplitSamples/TokenCounter.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.DeepDev;
+
+namespace SemanticKernelSandbox.TextSplitSamples;
+
+public static class TokenCounter
+{
+    private const string ImStart = "<|im_start|>";
+    private const string ImEnd = "<|im_end|>";
+
+    private static readonly Dictionary<string, int> SpecialTokens = new()
+    {
+        { ImStart,100264},
+        { ImEnd, 100265},
+    };
+
+    static TokenCounter()
+    {
+        DefaultTokenizer = TokenizerBuilder.CreateByModelNameAsync("gpt-3.5-turbo", SpecialTokens).GetAwaiter().GetResult();
+    }
+
+    private static ITokenizer DefaultTokenizer { get; }
+
+    private static readonly IReadOnlyCollection<string> AllowedSpecial = new HashSet<string>(SpecialTokens.Keys);
+
+    public static async Task<int> CountTokenAsync(string text, string modelName = "gpt-3.5-turbo")
+    {
+        if (modelName == "gpt-3.5-turbo")
+        {
+            var input = $"{ImStart}{text}{ImEnd}";
+            var encoded = DefaultTokenizer.Encode(input, AllowedSpecial);
+            return encoded.Count - 2;
+        }
+        else
+        {
+            var tokenizer = await TokenizerBuilder.CreateByModelNameAsync(modelName, SpecialTokens);
+            var input = $"{ImStart}{text}{ImEnd}";
+            var encoded = tokenizer.Encode(input, AllowedSpecial);
+            return encoded.Count - 2;
+        }
+    }
+}
